622a83ff41a301d615cd6ce9cdea41c427ef3434
neoraven <e.gezer@tu-bs.de>
2021-02-02 01:20:14 +0100

    ...

--- Implementation/utils.py
+++ Implementation/utils.py
@@ -75,14 +75,12 @@ class ETDData(Dataset):
             self.samples = np.asarray(self.data.loc[:, self.data.columns != 'atd'])
 	#Normalize 
         #self.normalized_samples = (self.samples - np.mean(self.samples, axis=0)) / np.std(self.samples, axis=0)
-        self.normalized_samples = (self.samples - self.samples.min(axis=1)) / (self.samples - self.samples.max(axis=1)    
+        self.normalized_samples = (self.samples - self.samples.min(axis=1)) / (self.samples - self.samples.max(axis=1))
         # Set up labels
         if objective == "ae":
             self.labels = np.asarray(self.normalized_samples)
         else:
             self.labels = np.asarray(self.data['atd'] - self.data['etd'])
-		
-        
  
     def __len__(self):
         return len(self.data)
