f5b3bc62726c3214e8e0560fb236ddb43bb8a53c
neoraven <e.gezer@tu-bs.de>
2021-02-07 18:18:46 +0100

    ...

--- Implementation/model.py
+++ Implementation/model.py
@@ -25,21 +25,26 @@ class Autoencoder(torch.nn.Module):
 
 
 class Regressor(torch.nn.Module):
-    def __init__(self, n_features, n_layers, factor):
+    def __init__(self, n_features, n_layers, n_neurons_hidden):
         super(Regressor, self).__init__()
         self.mode = "regression"
         self.view = 1
         self.layers = []
-        self.neurons = n_features
-        print(f"self.neurons init: {self.neurons}")
+        self.input_neurons = n_features
+        self.current_neurons = 0
+        self.prev_neurons = 0
+        print(f"self.neurons init: {self.input_neurons}")
         if n_layers != 0:
             for layer in range(n_layers):
-                self.layers.append(torch.nn.Linear(self.neurons, math.ceil(self.neurons*factor)))
+                self.current_neurons = n_neurons_hidden[layer]
+                self.prev_neurons = n_neurons_hidden[layer-1]
+                self.layers.append(torch.nn.Linear(self.prev_neurons, self.current_neurons))
                 self.layers.append(torch.nn.LeakyReLU())
-                self.neurons = math.ceil(self.neurons*factor)
-                print(f"self.neurons for hidden layer {layer+1}: {self.neurons}\n")
-        self.layers.append(torch.nn.Linear(self.neurons, 1))
-        self.main = torch.nn.Sequential(*self.layers)
+                print(f"number of neurons for hidden layer {layer+1}: {self.current_neurons}\n")
+            self.layers.append(torch.nn.Linear(self.current_neurons, 1))
+        else:
+            self.layers.append(torch.nn.Linear(self.input_neurons, 1))
+        self.net = torch.nn.Sequential(*self.layers)
 
     def forward(self, x):
-        return self.main(x)
+        return self.net(x)
